2afcda1535781a24effbb4bcdde0b9901bd7e678
kaoruheanna <kaoruheanna@gmail.com>
2017-09-19 21:35:14 -0300

    usuario

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # geop1
+Para este ejercicio uso el framework Yii como base.
+http://www.yiiframework.com
+
+The Active Record attributes are named after the associated table columns in a case-sensitive manner. Yii automatically defines an attribute in Active Record for every column of the associated table. You should NOT redeclare any of the attributes. 
---
+++ usuario.php
@@ -0,0 +1,42 @@
+<?php
+class Usuario extends CActiveRecord
+{
+	const EDAD_MINIMA = 18;
+
+    public static function model($className=__CLASS__){
+        return parent::model($className);
+    }
+ 
+    public function tableName() {
+        return 'USUARIOS';
+    }
+
+    public function primaryKey() {
+	    return 'codigousuario';
+	}
+
+	public function rules() {
+    	return array(
+            array('codigousuario, usuario', 'required'),
+            array('edad', 'integer'),
+            array('edad', 'esAdulto')
+        );
+    }
+
+    public function esAdulto($attribute,$params){
+    	if ($this->edad <= self::EDAD_MINIMA){
+    		$this->addError('edad','Edad invalida');
+    	}
+    }
+
+    public function relations() {
+        return array(
+        	// Para favoritos puede ser necesario desambiguar en cual de las dos columnas buscar el id para la consulta
+            'favoritos'=>array(self::MANY_MANY, 'Usuario','FAVORITOS(codigousuario, codigousuariofavorito)'),
+            'pagos'=>array(self::MANY_MANY, 'Pago','USUARIOSPAGOS(codigopago, codigousuario)')
+        );
+    }
+
+    TODO VALIDACION FOREIGN KEY DE FAVORITOS Y PAGOS
+
+}
